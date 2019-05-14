f54ab441f73d0c5fa8ea48109b95a2830fdf8bd3
Ben Parisot <me@benparisot.com>
2019-05-14 13:50:16 -0700

    write colorpicker and colordisplay

--- components/ColorPicker.js
+++ components/ColorPicker.js
@@ -1,14 +1,23 @@
 import React, { PureComponent } from 'react';
+import ColorDisplay from './ColorDisplay';
 
 export default class ColorPicker extends PureComponent {
- 
-  render() {
-    return (
+ state = {
+   selectedColor: ''
+ }
+
+ clickHandler = event => {
+   this.setState({ selectedColor: event.target.textContent }); 
+ }
+
+ render() {
+   return (
             <>
-            <button onClick={this.redHandler} style={{ background:'red' }}>Red</button>
-            <button onClick={this.blueHandler} style={{ background:'blue' }}>Blue</button>
-            <button onClick={this.yellowHandler} style={{ background:'yellow' }}>Yellow</button>
+            <button onClick={this.clickHandler} style={{ background:'red' }}>Red</button>
+            <button onClick={this.clickHandler} style={{ background:'blue' }}>Blue</button>
+            <button onClick={this.clickHandler} style={{ background:'yellow' }}>Yellow</button>
+            <ColorDisplay color={this.state.selectedColor} />
           </>
-    );
-  }
+   );
+ }
 }
