6efcc6c52bfa5f8b08e3b56947f0fbbbe500eac0
Ben Parisot <me@benparisot.com>
2019-05-14 11:09:56 -0700

    get color picker display to work

--- react-state/components/ColorPicker.js
+++ react-state/components/ColorPicker.js
@@ -1,14 +1,24 @@
 import React, { PureComponent } from 'react';
 
 export default class ColorPicker extends PureComponent {
- 
-  render() {
-    return (
-            <>
-            <button onClick={this.redHandler} style={{ background:'red' }}>Red</button>
-            <button onClick={this.blueHandler} style={{ background:'blue' }}>Blue</button>
-            <button onClick={this.yellowHandler} style={{ background:'yellow' }}>Yellow</button>
-          </>
-    );
-  }
+    state = {
+      background: 'grey',
+      height: '100px',
+      width: '100px'
+    }
+
+    clickHandler = event => {
+      this.setState({ background: event.target.textContent });
+    }
+    
+    render() {
+      return (
+        <>
+            <button onClick={this.clickHandler} style={{ background:'red' }}>Red</button>
+            <button onClick={this.clickHandler} style={{ background:'blue' }}>Blue</button>
+            <button onClick={this.clickHandler} style={{ background:'yellow' }}>Yellow</button>
+            <div style={this.state}></div>
+        </>
+      );
+    }
 }
